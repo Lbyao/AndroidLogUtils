0eed621c3684856c677eb003f1a455dd4045851a
lby <1483587277@qq.com>
2022-08-29 17:18:08 +0800

    添加引用测试

---
+++ app/src/main/java/com/liubuyao/androidlogutils/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.liubuyao.androidlogutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.liubuyao.utils.LogUtils
+
+/**
+ * @author lby
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
