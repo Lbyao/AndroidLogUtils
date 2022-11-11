7965e63da9744be16b45b5d2427945a9d41db982
lby <1483587277@qq.com>
2022-11-11 18:04:09 +0800

    1、优化权限请求工具类

--- utils/src/main/java/com/liubuyao/utils/PermissionsUtils.kt
+++ utils/src/main/java/com/liubuyao/utils/PermissionsUtils.kt
@@ -1,12 +1,21 @@
 package com.liubuyao.utils
 
+import android.Manifest
 import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.liubuyao.utils.bluetoothUtils.BluetoothHelper
+import java.lang.ref.WeakReference
 
 /**
  * @author lby
  */
 object PermissionsUtils {
+
+    lateinit var systemLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var mContext: WeakReference<ComponentActivity>
 
     /**
      * 是否获取到权限
@@ -29,9 +38,24 @@ object PermissionsUtils {
      */
     fun getIsGrantedPermission(permission: String): Boolean {
         return ContextCompat.checkSelfPermission(
-            MyUtils.getApp(),
-            permission
+            MyUtils.getApp(), permission
         ) == PackageManager.PERMISSION_GRANTED
     }
 
+    fun requestPermission(vararg permissions: String, context: ComponentActivity) {
+        systemLauncher = mContext.get()!!.registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { result ->
+            result?.let {
+                ToastUtils.showToast("result:$it")
+                for (permission in permissions){
+                    val b = it[permission]
+                    if (b != true) {
+                        ToastUtils.showToast("权限获取失败:$permission")
+                    }
+                }
+            }
+        }
+        systemLauncher.launch(permissions)
+    }
 }
